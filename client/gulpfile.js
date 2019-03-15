7f07a50d490d2b61f2f3c0aed69e855ec323ae5a
hbbaly <1422699902@qq.com>
2019-03-15 22:00:42 +0800

    ADD:使用gulp-util使输出的log有颜色

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp')
 var uglify  = require('gulp-uglify')
+var gutil = require('gulp-util')
 gulp.task('script',function(){
   gulp.src('public/js/*.js')
   .pipe(uglify())
   .pipe(gulp.dest('../server/public/js'))
 })
-gulp.task('default',['script'])
+gulp.task('default',function(){
+  gutil.log(gutil.colors.red('打包完成'))
+  gutil.log(gutil.colors.green('打包完成'))
+})
